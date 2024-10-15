60f06bcd0ad0403930c7b1e1001ed0277c2fb743
Zamarron <emilio.zamarrons@gmail.com>
2024-10-14 18:28:14 -0600

    W04 exercise

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Concurrent;
+using System.Diagnostics;
 
 public class LinkedList : IEnumerable<int>
 {
@@ -33,6 +35,15 @@ public class LinkedList : IEnumerable<int>
     public void InsertTail(int value)
     {
         // TODO Problem 1
+        Node newTail = new(value);
+        if(_tail is null){
+            _tail = newTail;
+            _head = newTail;
+        } else{
+            newTail.Prev = _tail;
+            _tail.Next = newTail;
+            _tail = newTail;
+        }
     }
 
 
@@ -65,6 +76,14 @@ public class LinkedList : IEnumerable<int>
     public void RemoveTail()
     {
         // TODO Problem 2
+        if(_tail == _head){
+            _tail = null;
+            _head = null;
+        }
+        else if(_tail is not null){
+                _tail.Prev!.Next = null;
+                _tail = _tail.Prev;
+        }
     }
 
     /// <summary>
@@ -109,7 +128,29 @@ public class LinkedList : IEnumerable<int>
     public void Remove(int value)
     {
         // TODO Problem 3
-    }
+        Node? curr =_head;
+        
+        while (curr != null) {
+            try{
+                
+                if(curr.Data == value){
+                if(curr == _head){
+                    RemoveHead();
+                }
+                else if(curr == _tail){
+                    RemoveTail();
+                }else{
+                    curr.Prev!.Next = curr.Next;
+                    curr.Next!.Prev = curr.Prev;
+                }
+                }            
+            }
+        catch {
+                Debug.WriteLine("The value is not in the linklist (Remove)");
+                return;
+            }
+        curr = curr.Next;
+    } }
 
     /// <summary>
     /// Search for all instances of 'oldValue' and replace the value to 'newValue'.
@@ -117,6 +158,30 @@ public class LinkedList : IEnumerable<int>
     public void Replace(int oldValue, int newValue)
     {
         // TODO Problem 4
+        Node? curr = _head;
+        Node? newNode = new(newValue);
+        while(curr != null){
+            try{
+                if(curr.Data == oldValue){
+                    curr.Data = newValue;
+                } 
+                else if(curr == _tail){
+                    
+                    InsertTail(newValue);
+                } else{
+                    curr.Prev!.Next = newNode;
+                    curr.Next!.Prev = curr.Prev;
+                    curr.Prev = newNode;
+                    curr.Next = curr.Next!.Next;
+                    curr.Next!.Prev = curr;
+                }
+                return;
+            } catch{
+                Debug.WriteLine("The value is not in the linklist");
+                break;
+            }
+            // curr = curr.Next;
+        }
     }
 
     /// <summary>
@@ -147,7 +212,13 @@ public class LinkedList : IEnumerable<int>
     public IEnumerable Reverse()
     {
         // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+        var curr = _tail; 
+        while (curr is not null)
+        {
+            yield return curr.Data; 
+            curr = curr.Prev; 
+        }
+        
     }
 
     public override string ToString()
